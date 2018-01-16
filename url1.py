6be4ae84c2ddcd02fcd7706ebe84cdd394c11bea
shaoqi <1511989705@qq.com>
2018-01-16 10:26:39 +0800

    链式调用

--- url1.py
+++ url1.py
@@ -3,10 +3,12 @@ class Chain(object):
     def __init__(self, path=''):
         self._path = path
 
-    def __getattr__(self, path):
+    def __getattr__(self, path):    	    
         return Chain('%s/%s' % (self._path, path))
 
     def __str__(self):
         return self._path
 
     __repr__ = __str__
+    __call__ = __getattr__
+
